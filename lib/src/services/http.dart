44b82d2ee52da4e39b81cb9f6e918a890e254509
viktorvoltz <vctrchinyeaka@gmail.com>
2022-03-10 10:37:36 +0100

    loading from cache

--- lib/src/services/http.dart
+++ lib/src/services/http.dart
@@ -6,37 +6,44 @@ import 'httpStatus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:convert';
 
-
 class CoinGeckoData {
   static Future<Object> getData() async {
-
     String coinFile = "coindata.json";
     var dir = await getTemporaryDirectory();
 
-    File file =  File(dir.path+"/"+coinFile);
+    File file = File(dir.path + "/" + coinFile);
 
-    if(file.existsSync()){
+    if (file.existsSync()) {
       var jsonData = file.readAsStringSync();
-      Success res = Success(response: coinGeckoFromJson(jsonData));
+      Success res = Success(
+        response: coinGeckoFromJson(jsonData),
+      );
       print('loading from cache');
       return res;
-    }else{
+    } else {
       try {
-      final response = await http.get(
-        Uri.parse(API_KEY),
-      );
-      if (response.statusCode == 200) {
-        print('loading from API');
-        file.writeAsStringSync(response.body, flush: true, mode: FileMode.write);
-        return Success(response: coinGeckoFromJson(response.body));
+        final response = await http.get(
+          Uri.parse(API_KEY),
+        );
+        if (response.statusCode == 200) {
+          print('loading from API');
+          file.writeAsStringSync(
+            response.body,
+            flush: true,
+            mode: FileMode.write,
+          );
+
+          return Success(
+            response: coinGeckoFromJson(response.body),
+          );
+        }
+      } on HttpException {
+        return Failure(code: 101, response: 'No Internet');
+      } on FormatException {
+        return Failure(code: 102, response: 'Invalid Format');
+      } catch (e) {
+        return Failure(code: 103, response: 'Invalid Response');
       }
-    } on HttpException {
-      return Failure(code: 101, response: 'No Internet');
-    } on FormatException {
-      return Failure(code: 102, response: 'Invalid Format');
-    } catch (e) {
-      return Failure(code: 103, response: 'Invalid Response');
-    }
     }
     return Failure(code: 100, response: 'Invalid Response');
   }
