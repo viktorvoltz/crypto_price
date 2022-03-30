d94fcbc63243b76affa2ac1873fb7b85af57512a
viktorvoltz <vctrchinyeaka@gmail.com>
2022-03-30 19:55:36 +0100

    initialize firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:coingecko/src/screens/cypto_list_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
