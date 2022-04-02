60d43738f6913eb8450024bb69dca494e577feec
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-02 07:29:08 +0100

    bloc

--- lib/src/blocs/coingecko_bloc.dart
+++ lib/src/blocs/coingecko_bloc.dart
@@ -1,6 +1,10 @@
 import 'package:coingecko/src/model/coingeckoModel.dart';
+import 'package:coingecko/src/screens/cypto_list_screen.dart';
 import 'package:coingecko/src/services/http.dart';
 import 'package:coingecko/src/services/httpStatus.dart';
+import 'package:coingecko/src/utils/authentication.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
 class CoingeckoBloc {
@@ -13,12 +17,24 @@ class CoingeckoBloc {
 
   Stream<List<CoinGecko>> get coinDataStream => _coinDataFetcher.stream;
 
+  User? _user;
+
+  User? get user => _user;
+
   setError(Failure error) {
     _error = error;
   }
 
-  setCoinList(List<CoinGecko> coinList){
+  setCoinList(List<CoinGecko> coinList) {
     _coinList = coinList;
+  }
+
+  Future<void> bSigninWithGoogle(BuildContext context) async {
+    _user = await Authentication.signInWithGoogle(context: context).whenComplete(() {
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+        return const CryptoList();
+      }));
+    });
   }
 
   getCoinData() async {
@@ -27,6 +43,7 @@ class CoingeckoBloc {
       List<CoinGecko> coinGecko = response.response!;
       _coinDataFetcher.sink.add(coinGecko);
       _coinList = [...coinGecko];
+
       ///_coinList.add(coinGecko as CoinGecko);
       setCoinList(_coinList);
     } else if (response is Failure) {
