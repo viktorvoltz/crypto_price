967e8d4980f97c9f21769f1ac39024125cef8641
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-03 15:53:27 +0100

    auth check

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GoogleSignIn().currentUser == null ? const AuthScreen() : const CryptoList(),
+      home: GoogleSignIn().currentUser == null && FirebaseAuth.instance.currentUser == null? const AuthScreen() : const CryptoList(),
     );
   }
 }
