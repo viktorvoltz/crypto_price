46f4ffc7d3d8bedb054c222db919f740bddcf7dd
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-06 18:50:21 +0100

    detail screen

---
+++ lib/src/screens/crypto_list_detail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CryptoDetail extends StatelessWidget {
+  const CryptoDetail({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
