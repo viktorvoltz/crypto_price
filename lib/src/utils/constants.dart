6aa38a016676208321bb71628e55b69fd50e09b8
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-10 15:33:04 +0100

    price color themes

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+
 const String API_KEY = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
+/// price change color themes
+const Color positive = Color.fromARGB(255, 3, 150, 8);
+const Color negative = Color.fromARGB(255, 207, 24, 11);
