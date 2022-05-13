ef4e6e5d3a5ccea9307f6f5cd2fd988ebf91da9d
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-13 13:55:46 +0100

    slider

--- lib/src/screens/crypto_list_detail.dart
+++ lib/src/screens/crypto_list_detail.dart
@@ -1,18 +1,43 @@
 import 'package:coingecko/src/model/coingeckoModel.dart';
 import 'package:flutter/material.dart';
 
-class CryptoDetail extends StatelessWidget {
+class CryptoDetail extends StatefulWidget {
   final CoinGecko? detail;
-  const CryptoDetail({this.detail, Key? key }) : super(key: key);
+  const CryptoDetail({this.detail, Key? key}) : super(key: key);
 
+  @override
+  State<CryptoDetail> createState() => _CryptoDetailState();
+}
+
+class _CryptoDetailState extends State<CryptoDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(detail!.name.toString()),
+        title: Text(widget.detail!.name.toString()),
       ),
       body: Center(
-        child: Text("Market Cap: \$${detail!.marketCap.toString()}"),
+        child: Column(
+          children: [
+            Text("Market Cap: \$${widget.detail!.marketCap.toString()}"),
+            Text("total volume: \$${widget.detail!.totalVolume.toString()}"),
+            SliderTheme(
+              data: const SliderThemeData(
+                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0),
+                showValueIndicator: ShowValueIndicator.always,
+              ),
+              child: Slider(
+                value: widget.detail!.currentPrice!,
+                max: widget.detail!.ath!,
+                min: widget.detail!.atl!,
+                onChanged: (double value){
+                  
+                },
+                label: widget.detail!.currentPrice.toString(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
