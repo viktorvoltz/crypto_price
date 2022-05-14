16767b3fdd2ac8a6fbf3f85864a7eaa2121cd27c
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-14 12:29:06 +0100

    improved

--- lib/src/widget/drawer.dart
+++ lib/src/widget/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:coingecko/src/blocs/coingecko_bloc.dart';
 import 'package:coingecko/src/screens/authscreen.dart';
 import 'package:coingecko/src/utils/authentication.dart';
+import 'package:coingecko/src/widget/drawer_items.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -48,37 +49,7 @@ class DrawerWidget extends StatelessWidget {
                 ],
               ),
             ),
-            ListTile(
-                leading: const Icon(
-                  Icons.exit_to_app,
-                  color: Colors.red,
-                ),
-                title: const Text(
-                  "Sign Out",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.red,
-                  ),
-                ),
-                onTap: () {
-                  try {
-                    bloc.bSignOut(context).whenComplete(() {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return const AuthScreen();
-                          },
-                        ),
-                      );
-                    });
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      Authentication.customSnackBar(
-                        content: 'Error signing out. Try again.',
-                      ),
-                    );
-                  }
-                }),
+            const DrawerItem(),
           ],
         ),
       );
