ed2302522551f415d9758aa27b2cdb13e3dea40e
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-14 12:27:06 +0100

    starred prefs

--- lib/src/blocs/busyHandler.dart
+++ lib/src/blocs/busyHandler.dart
@@ -4,28 +4,40 @@ import 'package:coingecko/src/screens/cypto_list_screen.dart';
 import 'package:coingecko/src/utils/authentication.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BusyHandler extends ChangeNotifier{
   User? _user;
   bool _isbusy = false;
   bool _refStatus = true;
-  List<CoinGecko> starredList = [];
+  final List<CoinGecko> _starredList = [];
 
   bool get isbusy => _isbusy;
   bool get refStatus => _refStatus;
+  List<CoinGecko> get starredList => _starredList;
 
   void refreshStatus(){
     _refStatus = !_refStatus;
     notifyListeners();
   }
 
-  void addToStarredList(CoinGecko item){
-    starredList.add(item);
+  Future<List<CoinGecko>> getStarredList() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String list = prefs.getString("fav_coins") ?? "null";
+    final List<CoinGecko> coinList = coinGeckoFromJson(list);
+    return coinList;
+  }
+
+  void addToStarredList(CoinGecko item) async{
+    _starredList.add(item);
     notifyListeners();
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String stringData = coinGeckoToJson(_starredList);
+    await prefs.setString("fav_coins", stringData);
   }
 
   void removeFromStarredList(CoinGecko item){
-    starredList.remove(item);
+    _starredList.remove(item);
     notifyListeners();
   }
 
