9b907941acd810c16552d3fc02992f61e620e122
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-29 15:07:03 +0100

    dark theme pref

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:coingecko/src/blocs/busyHandler.dart';
+import 'package:coingecko/src/blocs/theme_provider.dart';
+import 'package:coingecko/src/model/themes.dart';
 import 'package:coingecko/src/screens/authscreen.dart';
 import 'package:coingecko/src/screens/cypto_list_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,28 +10,52 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import './src/utils/authentication.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeProvider themeProvider = new ThemeProvider();
+
+  @override
+  void initState() {
+    getCurrentTheme();
+    super.initState();
+  }
+
+  void getCurrentTheme() async {
+    themeProvider.darkTheme = await themeProvider.themePreference.getTheme();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<BusyHandler>(
-      create: (_) => BusyHandler(),
-      child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Crypto App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: GoogleSignIn().currentUser == null && FirebaseAuth.instance.currentUser == null? const AuthScreen() : const CryptoList(),
-    ));
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => BusyHandler()),
+          ChangeNotifierProvider(create: (_) => themeProvider),
+        ],
+        child: Consumer<ThemeProvider>(
+          builder: (ctx, value, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Crypto App',
+              theme: Themes.themeData(themeProvider.darkTheme, context),
+              home: GoogleSignIn().currentUser == null &&
+                      FirebaseAuth.instance.currentUser == null
+                  ? const AuthScreen()
+                  : const CryptoList(),
+            );
+          },
+        ));
   }
 }
-
