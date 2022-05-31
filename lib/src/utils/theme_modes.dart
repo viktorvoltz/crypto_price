e9f4ebcde35007ce3cdd57b80ef905bd35edd685
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-31 13:59:39 +0100

    themes

--- lib/src/utils/theme_modes.dart
+++ lib/src/utils/theme_modes.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.black,
-    brightness: Brightness.dark,
-    backgroundColor: const Color(0xFF212121),
-    accentColor: Colors.white,
-    dividerColor: Colors.black12,
-    iconTheme: const IconThemeData(color: Colors.white),
-    primaryIconTheme: const IconThemeData(color: Colors.black));
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.black,
+  brightness: Brightness.dark,
+  backgroundColor: const Color(0xFF212121),
+  accentColor: Colors.white,
+  dividerColor: Colors.black12,
+  iconTheme: const IconThemeData(color: Colors.white),
+  primaryIconTheme: const IconThemeData(color: Colors.black),
+);
 
 final lightTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.white,
-    brightness: Brightness.light,
-    backgroundColor: const Color(0xFFE5E5E5),
-    accentColor: Colors.black,
-    dividerColor: Colors.grey,
-    iconTheme: const IconThemeData(color: Colors.black),
-    primaryIconTheme: const IconThemeData(color: Colors.black));
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.white,
+  brightness: Brightness.light,
+  backgroundColor: const Color(0xFFE5E5E5),
+  accentColor: Colors.black,
+  dividerColor: Colors.grey,
+  iconTheme: const IconThemeData(color: Colors.black),
+  primaryIconTheme: const IconThemeData(color: Colors.black),
+);
