8050f2913470f1eb0e543c0484d256cfe8e4aff6
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-31 13:47:08 +0100

    initial theme load bug fix

--- lib/src/blocs/theme_provider.dart
+++ lib/src/blocs/theme_provider.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:coingecko/src/utils/theme_pref.dart';
+import 'package:coingecko/src/utils/theme_modes.dart';
 
 class ThemeProvider extends ChangeNotifier{
-  final darkTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.black,
-    brightness: Brightness.dark,
-    backgroundColor: const Color(0xFF212121),
-    accentColor: Colors.white,
-    accentIconTheme: IconThemeData(color: Colors.black),
-    dividerColor: Colors.black12,
-  );
 
-  final lightTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.white,
-    brightness: Brightness.light,
-    backgroundColor: const Color(0xFFE5E5E5),
-    accentColor: Colors.black,
-    accentIconTheme: IconThemeData(color: Colors.white),
-    dividerColor: Colors.white54,
-  );
-
-  ThemeData? _themeData;
+  static ThemeData? _themeData;
   ThemeData getTheme() => _themeData!;
 
-  ThemeProvider() {
-    ThemePreference.readData('themeMode').then((value) {
+  ThemeProvider();
+
+  /// sets the App's theme on start up.
+  Future<void> setTheme()async{
+    await ThemePreference.readData('themeMode').then((value) {
       print('value read from storage: ' + value.toString());
       var themeMode = value ?? 'light';
       if (themeMode == 'light') {
         _themeData = lightTheme;
       } else {
-        print('setting dark theme');
         _themeData = darkTheme;
       }
       notifyListeners();
