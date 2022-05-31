cf4cfc98ec1405ed23caeb9dfb5ea89bb3d06a4e
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-31 13:46:40 +0100

    initial theme load bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:coingecko/src/blocs/theme_provider.dart';
 import 'package:coingecko/src/model/themes.dart';
 import 'package:coingecko/src/screens/authscreen.dart';
 import 'package:coingecko/src/screens/cypto_list_screen.dart';
+import 'package:coingecko/src/utils/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -13,7 +14,12 @@ import './src/utils/authentication.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  ThemeProvider themeProvider = ThemeProvider();
+  await themeProvider.setTheme();
+  runApp(MultiProvider(
+    providers: providers,
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -27,23 +33,18 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => BusyHandler()),
-          ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ],
-        child: Consumer<ThemeProvider>(
-          builder: (ctx, value, child) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Crypto App',
-              theme: value.getTheme(),
-              home: GoogleSignIn().currentUser == null &&
-                      FirebaseAuth.instance.currentUser == null
-                  ? const AuthScreen()
-                  : const CryptoList(),
-            );
-          },
-        ));
+    return Consumer<ThemeProvider>(
+      builder: (ctx, value, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Crypto App',
+          theme: value.getTheme(),
+          home: GoogleSignIn().currentUser == null &&
+                  FirebaseAuth.instance.currentUser == null
+              ? const AuthScreen()
+              : const CryptoList(),
+        );
+      },
+    );
   }
 }
