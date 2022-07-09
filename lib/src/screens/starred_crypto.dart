73aa51c706a3e871b9cca527995e88e79ac49bc2
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-09 16:55:22 +0100

    empty favourites fix

--- lib/src/screens/starred_crypto.dart
+++ lib/src/screens/starred_crypto.dart
@@ -18,59 +18,60 @@ class StarredCrypto extends StatelessWidget {
       appBar: AppBar(
         title: Text("${starredList.length} Starred Assets"),
       ),
-      body: ListView.builder(
-        itemCount: starredList.length,
-        itemBuilder: (context, index) {
-          if (starredList.isEmpty) {
-            return const Center(
-              child: Text("Your favourite coins will appear here."),
-            );
-          }
-          return ListTile(
-            leading: SizedBox(
-              width: 40,
-              height: 40,
-              child: CachedNetworkImage(
-                width: 40,
-                height: 40,
-                fit: BoxFit.contain,
-                imageUrl: starredList[index].image.toString(),
-                progressIndicatorBuilder: (context, url, downloadProgress) =>
-                    CircularProgressIndicator(value: downloadProgress.progress),
-                errorWidget: (context, url, error) => const Icon(
-                  Icons.error,
-                  size: 30,
-                  color: negative,
-                ),
-              ),
-            ),
-            title: GestureDetector(
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return CryptoDetail(
-                    detail: starredList[index],
-                  );
-                }));
+      body: starredList.isEmpty
+          ? const Center(
+            child: Text("Your favourite coins will appear here."),
+          )
+          : ListView.builder(
+              itemCount: starredList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: CachedNetworkImage(
+                      width: 40,
+                      height: 40,
+                      fit: BoxFit.contain,
+                      imageUrl: starredList[index].image.toString(),
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) =>
+                              CircularProgressIndicator(
+                                  value: downloadProgress.progress),
+                      errorWidget: (context, url, error) => const Icon(
+                        Icons.error,
+                        size: 30,
+                        color: negative,
+                      ),
+                    ),
+                  ),
+                  title: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return CryptoDetail(
+                          detail: starredList[index],
+                        );
+                      }));
+                    },
+                    child: Text(starredList[index].name.toString()),
+                  ),
+                  subtitle: Text(
+                    '\$' +
+                        starredList[index].currentPrice.toString() +
+                        "   "
+                            '(' +
+                        starredList[index].symbol.toString().toUpperCase() +
+                        ')',
+                    overflow: TextOverflow.clip,
+                  ),
+                  trailing: PriceChange(
+                    data: starredList,
+                    index: index,
+                  ),
+                );
               },
-              child: Text(starredList[index].name.toString()),
             ),
-            subtitle: Text(
-              '\$' +
-                  starredList[index].currentPrice.toString() +
-                  "   "
-                      '(' +
-                  starredList[index].symbol.toString().toUpperCase() +
-                  ')',
-              overflow: TextOverflow.clip,
-            ),
-            trailing: PriceChange(
-              data: starredList,
-              index: index,
-            ),
-          );
-        },
-      ),
     );
   }
 }
