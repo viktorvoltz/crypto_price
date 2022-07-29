b85f57689344ee1977578ae32d13f01e16e8f00e
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-29 11:07:21 +0100

    refactor

--- lib/src/screens/crypto_list_detail.dart
+++ lib/src/screens/crypto_list_detail.dart
@@ -165,32 +165,6 @@ class _CryptoDetailState extends State<CryptoDetail> {
               padding: const EdgeInsets.only(left: 10),
               child: Text("Low: ${widget.detail!.low24H.toString()}"),
             ),
-            /*Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Text("ATL: ${widget.detail!.atl.toString()}"),
-                ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text("ATH: ${widget.detail!.ath.toString()}"),
-                ),
-              ],
-            ),*/
-            /*SliderTheme(
-              data: const SliderThemeData(
-                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0),
-                showValueIndicator: ShowValueIndicator.always,
-              ),
-              child: Slider(
-                value: widget.detail!.currentPrice!,
-                max: widget.detail!.ath!,
-                min: widget.detail!.atl!,
-                onChanged: (double value) {},
-                label: widget.detail!.currentPrice.toString(),
-              ),
-            )*/
           ],
         ),
       ),
